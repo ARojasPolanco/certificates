8a7dcd689788b6f4bd1be22ff81c3b9694d456c7
ARojasPolanco <advbrrop23@gmail.com>
2023-11-27 19:51:11 -0300

    add certificates

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import security from '../public/images/Seguridad_informatica.jpg'
 import secutity2 from '../public/images/Seguridad_informatica_2.jpg'
 import tailwind from '../public/images/Tailwind.jpg'
 import js from '../public/images/js.jpg'
+import css from '../public/images/css.jpg'
+import html from '../public/images/html.jpg'
 
 function App() {
 
@@ -17,6 +19,8 @@ function App() {
         <img className='h-[250px] shadow-lg' src={fundamentos} alt="" />
         <img className='h-[250px] shadow-lg' src={fullstack} alt="" />
         <img className='h-[250px] shadow-lg' src={js} alt="" />
+        <img className='h-[250px] shadow-lg' src={css} alt="" />
+        <img className='h-[250px] shadow-lg' src={html} alt="" />
         <img className='h-[250px] shadow-lg' src={tailwind} alt="" />
         <img className='h-[250px] shadow-lg' src={frontend} alt="" />
         <img className='h-[250px] shadow-lg' src={security} alt="" />
